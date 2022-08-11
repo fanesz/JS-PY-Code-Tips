401e72793ece68810247b1528ff7f1f8a347bbf7
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-08-11 14:09:20 +0700

    Create Sorting.py

---
+++ Basic Python/Sorting.py
@@ -0,0 +1,10 @@
+def urutlist(arr, terbalik = False) :
+    arr.sort (reverse=terbalik)
+
+arr = [1, 2, 3, 4, 5]
+
+print(urutlist(arr, True))
+>>> [5, 4, 3, 2, 1]
+
+#work with string array
+#only work with array
