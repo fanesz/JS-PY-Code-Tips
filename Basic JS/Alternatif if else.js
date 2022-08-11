d97587e0ac80f37d6b46e004320ac4e4429d6e67
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-08-11 13:34:48 +0700

    Create Alternatif if else.js

---
+++ Basic JS/Alternatif if else.js
@@ -0,0 +1,11 @@
+//normal if else :
+if(gender == 'female'){
+    const message = 'Hey Girl!'
+} else {
+    const message = 'Hey Boy!'
+}
+
+
+//better way :
+const message = 
+  	gender == 'female' ? 'Hey Girl!' : 'Hey Boy!'
