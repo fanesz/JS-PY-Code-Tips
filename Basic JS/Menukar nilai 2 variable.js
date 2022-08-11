46d27b399cd5617534b7151306a3f9d2ed0f6a47
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-08-11 13:37:44 +0700

    Create Menukar nilai 2 variable.js

---
+++ Basic JS/Menukar nilai 2 variable.js
@@ -0,0 +1,11 @@
+// metode simpel dengan menambah 1 lagi variabel, tapi ini metode tanpa menambah variable
+
+var a = 7
+var b = 2
+console.log(a, b)
+
+a -= b
+b += a
+a = b - a
+
+console.log(a, b)
