65eb8210d8f097728cce8ed74ee918afd2e200ff
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-08-11 14:07:21 +0700

    Create Reverse function.py

---
+++ Basic Python/Reverse function.py
@@ -0,0 +1,12 @@
+def reverseInput(input):
+  return input[::-1]
+
+arr = testing
+print(reverseInput(arr))
+#output = gnitset
+
+arr = ['tes1', 'tes2']
+print(reverseInput(arr))
+#output = ['tes2', 'tes1']
+
+#work with array, string, int
