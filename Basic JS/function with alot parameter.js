db99df8a16521149890c4aca101159a75c4826b6
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-09-02 13:37:57 +0700

    Create function with alot parameter.js

---
+++ Basic JS/function with alot parameter.js
@@ -0,0 +1,14 @@
+function addEmployee(employee){
+    console.log(employee);
+    // karna ini object, employee.name = 'Fanes', employee.age = 19, dst...
+}
+
+const fanes = {
+    name: 'Fanes',
+    age: 19,
+    gender: 'Male',
+    country: 'Indonesia'
+}
+
+addEmployee(fanes)
+// { name: 'Fanes', age: 19, gender: 'Male', country: 'Indonesia' }
