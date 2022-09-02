60000579476f5cada347de9381f21d2124f15aec
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-09-02 18:52:21 +0700

    Create map() di array.js

---
+++ Basic JS/map() di array.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5]
+const newArr = numbers.map(
+    (number) => number * 2 //kali 2 untuk semua element di array
+)
+console.log(newArr);
+// [ 2, 4, 6, 8, 10 ]
+
+// mungkin bisa pakai for loop, tapi ini lebih simple
