52a973fa819ca587e37a71c474a8ed3f425a1c22
Vanezkun <91197642+Vanezkun@users.noreply.github.com>
2022-09-09 23:04:55 +0700

    Update Class Constructor.js

--- Basic JS/Class Constructor.js
+++ Basic JS/Class Constructor.js
@@ -4,14 +4,20 @@ class Company {
     constructor(name, country) {
         this.name = name
         this.country = country
+        this.price = 0
+    }
+    Pricing(price) {
+        this.price = price
     }
 }
 
-const tesla = new Company('Tesla', 'USA')
 const toyota = new Company('Toyota', 'Japan')
-
-
-console.log(tesla);
-// output = Company { name: 'Tesla', country: 'USA' }
 console.log(toyota);
-//output = Company { name: 'Toyota', country: 'Japan' }
+// Company { name: 'Toyota', country: 'Japan', price: 0 }
+
+
+toyota.Pricing(10)
+
+console.log(toyota);
+// Company { name: 'Toyota', country: 'Japan', price: 10 }
+
